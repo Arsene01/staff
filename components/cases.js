fe39f645e67cdc458e3a98d5067ae6cf6f44d01f
Arsene Kobelev <arsene01@yandex.ru>
2020-05-11 17:03:53 +0300

    modified cases.js and created test suite for it

--- components/cases.js
+++ components/cases.js
@@ -1,5 +1,11 @@
+exports.changeEnding = function changeEnding(element, endingSource, toCase) {
+  const { end, length } = getEndingObject(element);
+  const match = findMatch(endingSource, end);
+  if (!match) return null;
+  return element.substring(0, element.length - length) + match[getCase(toCase)];
+}
 
-function getCase(newCase) {
+exports.getCase = function getCase(newCase) {
   return [
     'nominative', 'dative', 'genitive', 'accusative'
   ].includes(newCase) ? newCase : 'nominative';
@@ -14,11 +20,4 @@ function getEndingObject(element) {
 }
 function findMatch(endingSource, end) {
   return endingSource.find(endingObject => endingObject.nominative === end);
-}
-
-exports.changeEnding = function changeEnding(element, endingSource, toCase) {
-  const { end, length } = getEndingObject(element);
-  const match = findMatch(endingSource, end);
-  if (!match) return null;
-  return element.substring(0, element.length - length) + match[getCase(toCase)];
 }
---
+++ tests/components/cases.test.js
@@ -0,0 +1,29 @@
+const { getCase, changeEnding } = require('../../components/cases.js');
+
+const cases = [
+  nominative: 'сен', dative: 'сену', accusative: 'сена', genitive: 'сена' },
+  nominative: 'нна', dative: 'нне', accusative: 'нну', genitive: 'нны' },
+  nominative: 'лав', dative: 'лаву', accusative: 'лава', genitive: 'лава' },
+  nominative: 'тыр', dative: 'тыру', accusative: 'тыра', genitive: 'тыра' },
+]
+
+describe("getCase method testing...", () => {
+  test("no arguments aplied", () => {
+    expect(getCase()).toEqual('nominative');
+  });
+  test("notValid argument aplied", () => {
+    expect(getCase()).toEqual('nominative');
+  });
+  test("'nominative' argument aplied", () => {
+    expect(getCase('nominative')).toEqual('nominative');
+  });
+  test("'dative' argument aplied", () => {
+    expect(getCase('dative')).toEqual('dative');
+  });
+  test("'accusative' argument aplied", () => {
+    expect(getCase('accusative')).toEqual('accusative');
+  });
+  test("'genitive' argument aplied", () => {
+    expect(getCase('genitive')).toEqual('genitive');
+  });
+});
