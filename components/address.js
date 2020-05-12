94765233ca221adb391797c1567459ba083cc565
Arsene Kobelev <arsene01@yandex.ru>
2020-05-12 21:39:21 +0300

    added registerAddress to Address class with test suite updating

--- components/address.js
+++ components/address.js
@@ -1,7 +1,7 @@
 module.exports = class Address {
   static getAddress(
-    dispatcher,
-    { regionId, areaId, rest }
+    { regionId, areaId, rest },
+    dispatcher
   ) {
     const address = [];
     const state = dispatcher.stateOf('address-data');
@@ -11,5 +11,12 @@ module.exports = class Address {
     if (areaId && region.areas) address.push(region.areas.find((a) => a.id === areaId).name);
     if (rest) address.push(rest);
     return address.join(', ');
+  }
+  static registerAddress(address, dispatcher) {
+    const state = dispatcher.stateOf('address-data');
+    const reg = address.split(', ');
+    const region = state.find((r) => r.name === reg[0]);
+    if (!region) return null;
+    return { regionId: region.id, rest: reg.slice(1).join(', ') };
   }
 }
--- tests/components/address.test.js
+++ tests/components/address.test.js
@@ -5,16 +5,37 @@ describe("Address class testing...", () => {
   const dispatcher = new Dispatcher();
   test("getAddress method testing", () => {
     const result = Address.getAddress(
-      dispatcher,
-      { regionId: 4, rest: 'г. Майкоп, ул. Юннатов, д. 2, кв. 64' }
+      { regionId: 4, rest: 'г. Майкоп, ул. Юннатов, д. 2, кв. 64' },
+      dispatcher
     );
     expect(result).toEqual('Республика Адыгея, г. Майкоп, ул. Юннатов, д. 2, кв. 64');
   });
   test("getAddress method testing", () => {
     const result = Address.getAddress(
-      dispatcher,
-      { regionId: 1, rest: 'ул. Садовая, д. 3' }
+      { regionId: 1, rest: 'ул. Садовая, д. 3' },
+      dispatcher
     );
     expect(result).toEqual('Москва, ул. Садовая, д. 3');
+  });
+  test("registerAddress method testing", () => {
+    const result = Address.registerAddress(
+      'Республика Адыгея, г. Майкоп, ул. Юннатов, д. 2, кв. 64',
+      dispatcher
+    );
+    expect(result).toEqual({ regionId: 4, rest: 'г. Майкоп, ул. Юннатов, д. 2, кв. 64' });
+  });
+  test("registerAddress method testing", () => {
+    const result = Address.registerAddress(
+      'Москва, ул. Садовая, д. 3',
+      dispatcher
+    );
+    expect(result).toEqual({ regionId: 1, rest: 'ул. Садовая, д. 3' });
+  });
+  test("registerAddress method testing", () => {
+    const result = Address.registerAddress(
+      'г. Майкоп, ул. Юннатов, д. 2, кв. 64',
+      dispatcher
+    );
+    expect(result).toEqual(null);
   });
 });
