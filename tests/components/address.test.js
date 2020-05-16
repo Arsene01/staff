86b839cb96ee5e7f915ed65b7d5406e181024c39
unknown <arsene01@yandex.ru>
2020-05-16 13:00:24 +0300

    added cities direcory

--- tests/components/address.test.js
+++ tests/components/address.test.js
@@ -2,7 +2,25 @@ const Address = require('../../components/address.js');
 const Dispatcher = require('../../dispatcher.js');
 
 describe("Address class testing...", () => {
-  const dispatcher = new Dispatcher();
+  const address = new Address(new Dispatcher());
+  test("registerAddress method testing", () => {
+    const result = address.registerAddress(
+      {
+        region: 'Республика Адыгея',
+        city: 'Майкоп',
+        street: 'Юннатов',
+        house: 2,
+        apartment: 64
+      }
+      { regionId: 4, rest: 'г. Майкоп, ул. Юннатов, д. 2, кв. 64' },
+      dispatcher
+    );
+    expect(result).toEqual({
+      regionId: 4,
+      cityId: 'Майкоп',
+    });
+  });
+  /*
   test("getAddress method testing", () => {
     const result = Address.getAddress(
       { regionId: 4, rest: 'г. Майкоп, ул. Юннатов, д. 2, кв. 64' },
@@ -10,6 +28,7 @@ describe("Address class testing...", () => {
     );
     expect(result).toEqual('Республика Адыгея, г. Майкоп, ул. Юннатов, д. 2, кв. 64');
   });
+
   test("getAddress method testing", () => {
     const result = Address.getAddress(
       { regionId: 1, rest: 'ул. Садовая, д. 3' },
@@ -37,5 +56,5 @@ describe("Address class testing...", () => {
       dispatcher
     );
     expect(result).toEqual(null);
-  });
+  });*/
 });
